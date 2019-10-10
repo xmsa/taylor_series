325d65e44a3b806f808b890b8684c62b21ec5656
mohammad mazaheri <53862625+xmsa@users.noreply.github.com>
2019-10-10 12:10:07 +0330

    add code Btn_Sinh_Click

--- taylor_series/MainWindow.xaml.cs
+++ taylor_series/MainWindow.xaml.cs
@@ -272,7 +272,29 @@ namespace taylor_series
 
         private void Btn_Sinh_Click(object sender, RoutedEventArgs e)
         {
+            double X = 0;
+            double N = 0;
+            bool x = get_value(txt_X, ref X);
+            bool n = get_value(txt_N, ref N);
+            if (x && n)
+            {
+                // sun ( x^(2n+1) / (2n+1)! )
+                // a = x^(2n+1)
+                // b = (2n+1)! 
+                // sun (a/b) 
 
+                double a = X, c = 1, b = 1, sum = X;
+
+                for (int i = 1; i < N; i++)
+                {
+                    a *= X * X;
+                    b *= ((2 * i) * (2 * i + 1));
+                    sum += (a / b);
+                }
+
+                MessageBox.Show(sum.ToString());
+
+            }
         }
 
         private void Btn_Cosh_Click(object sender, RoutedEventArgs e)
